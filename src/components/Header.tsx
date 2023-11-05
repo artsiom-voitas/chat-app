fa98bf957dbb2e1d261baeedff5c253e2302439b
Artsiom Voitas <artsiom.voitas@gmail.com>
2023-11-05 15:51:35 +0300

    feat: added routing via nextui components
    updated packages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { ThemeSwitcher } from '.';
 
 export default function Header() {
     return (
-        <header className="m-3 flex items-center justify-between">
+        <header className="mx-2 my-3 flex items-center justify-between sm:mx-0">
             <ThemeSwitcher />
             <UserButton afterSignOutUrl="/" />
         </header>
